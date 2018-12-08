ebe5712c8456a05b20d63a630291579908473e6c
“[Antti <[antti.ht.kosonen@gmail.com]>
2018-12-08 10:58:49 +0200

    Initiate app.

--- index.js
+++ index.js
@@ -44,9 +44,13 @@ app.use('/api/blogs', blogRouter)
 
 const server = http.createServer(app)
 
+server.listen(config.port || 3001)
+
+/*
 server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
 })
+*/
 
 server.on('close', () => {
   mongoose.connection.close()
